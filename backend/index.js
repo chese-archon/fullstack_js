f405fdbb0d7531144e2d4ea0db9216339dd89553
Anna <afgnkad@gmail.com>
2024-11-27 11:22:56 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+// backend/index.js
+const express = require('express');
+ 
+const PORT = process.env.PORT || 3456;//3010;
+//const PORT = process.env.PORT || 8000;
+const app = express();
+ 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+ 
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+//Следующий код создает эндпоинт /api/todo-items. React приложение будет отправлять GET запрос на этот эндпоинт.
+const todoItems = require('./api/todo-items.json');
+app.get('/api/todo-items', (req, res) => {
+  res.json({ data: todoItems });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
